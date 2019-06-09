c36ce44a4e345f9e894ad3924145a51713a19306
psmorandi <psmorandi@gmail.com>
2019-06-09 18:40:07 -0300

    Added the scripts. It's a first commit from my home machine. Needs to be updated with work machine extensions.

---
+++ powershell/install_extensions.ps1
@@ -0,0 +1,6 @@
+code --install-extension HookyQR.beautify
+code --install-extension jchannon.csharpextensions
+code --install-extension mathiasfrohlich.Kotlin
+code --install-extension ms-vscode.csharp
+code --install-extension ms-vscode.powershell
+code --install-extension PeterJausovec.vscode-docker
