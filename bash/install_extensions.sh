083af17f786503638121394a50248f6439c56387
Paulo Morandi <paulomorandi@mobiltec.com.br>
2022-06-03 12:53:05 -0300

    Updating list of vscode extensions.

--- bash/install_extensions.sh
+++ bash/install_extensions.sh
@@ -2,5 +2,15 @@ code --install-extension HookyQR.beautify
 code --install-extension jchannon.csharpextensions
 code --install-extension mathiasfrohlich.Kotlin
 code --install-extension ms-vscode.csharp
-code --install-extension ms-vscode.powershell
 code --install-extension PeterJausovec.vscode-docker
+code --install-extension cschlosser.doxdocgen
+code --install-extension jeff-hykin.better-cpp-syntax
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension ms-vscode.cpptools-themes
+code --install-extension twxs.cmake
